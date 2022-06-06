47ba97c63c823f03a7b8dc57f30894f722b8b4f6
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-06-06 10:48:19 -0500

    Init Commit

---
+++ src/main/java/com/example/vetted/VettedApplication.java
@@ -0,0 +1,13 @@
+package com.example.vetted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VettedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VettedApplication.class, args);
+    }
+
+}
