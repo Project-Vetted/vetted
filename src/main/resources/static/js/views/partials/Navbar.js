71d6fe9350c607e39a00f8ef6cbbfacf98b162de
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-06-06 15:24:40 -0500

    updated views for user register/login testing

---
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(props) {
+    return `
+        <nav>
+            <a href="/" data-link>Home</a>
+            <a href="/login" data-link>Login</a>
+            <a href="/register" data-link>Register</a>
+        </nav>
+    `;
+}
