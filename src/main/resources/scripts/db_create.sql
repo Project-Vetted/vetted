83815701efa22e304e84a4bed6c422a5d92e40bb
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-06-14 09:13:35 -0500

    Revert "Merge pull request #18 from Project-Vetted/dev-branch"
    This reverts commit 623f83ccde39d0ae839177a6536706632fffe315, reversing
    changes made to 58001bdc77dac9f1fb8b92928bf7ea19fe2e4904.

--- src/main/resources/scripts/db_create.sql
+++ src/main/resources/scripts/db_create.sql
@@ -1,3 +1,4 @@
+
 DROP DATABASE if exists vetted_db;
 
 CREATE DATABASE if not exists vetted_db;
@@ -10,33 +11,41 @@ CREATE TABLE users
     username VARCHAR(60)  NOT NULL,
     password VARCHAR(60)  NOT NULL,
     email    VARCHAR(200) NOT NULL,
+    photoUrl VARCHAR(200) NOT NULL,
     role     VARCHAR(32)  NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE categories
 (
-    id   BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(60),
+    id   INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE user_category
+CREATE TABLE user_categories
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id      BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
-    category_id BIGINT NOT NULL,
+    category_id  INT    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (category_id) REFERENCES categories (id)
 );
 
-CREATE TABLE user_point_interactions
+CREATE TABLE user_points
+(
+    user_id BIGINT NOT NULL,
+    points BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE user_friends
 (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    user_that_upvoted BIGINT NOT NULL,
-    user_upvoted BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    friend_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_that_upvoted) REFERENCES users (id),
-    FOREIGN KEY (user_upvoted) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (friend_id) REFERENCES users (id)
 );
--- src/main/resources/static/js/views/Loading.js
+++ src/main/resources/static/js/views/Loading.js
@@ -1,8 +1,3 @@
 export default function Loading(props) {
-    //language=HTML
-    return `
-        <div class="loader">
-            <img src="../../assets/imgs/load-gif.gif" alt="">
-        </div>
-    `;
+    return `<h1>Loading...</h1>`;
 }
