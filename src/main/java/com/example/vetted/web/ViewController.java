9a641ad17d2f09c110bb86929ea0214aa3ccdc3b
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-06-15 10:02:33 -0500

    importing user dashboard view & necessary files

--- src/main/java/com/example/vetted/web/ViewController.java
+++ src/main/java/com/example/vetted/web/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping({"/", "/about", "/login", "/home", "/register"})
+    @RequestMapping({"/", "/about", "/login", "/home", "/register", "/dashboard"})
     public String showView() {
         return "forward:/index.html";
     }
