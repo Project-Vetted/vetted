abe7e61b6a1d756e8d51c358892028c4a0b663a1
Will Roberts <willroberts2113@gmail.com>
2022-06-17 07:59:05 -0500

    syncing with dev2

--- src/main/resources/static/js/chatFunctions/loadChat.js
+++ src/main/resources/static/js/chatFunctions/loadChat.js
@@ -5,6 +5,8 @@ import fetchData from "../fetchData.js";
 
 export function loadChat (hostUser, connectingUser, roomAddress) {
 
+    // Video Chat Broken?
+
     (function(t,a,l,k,j,s){
         s=a.createElement('script');s.async=1;s.src="https://cdn.talkjs.com/talk.js";a.head.appendChild(s)
         ;k=t.Promise;t.Talk={v:3,ready:{then:function(f){if(k)return new k(function(r,e){l.push([f,r,e])});l
