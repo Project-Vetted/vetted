486bce467e5cbef8edb184b187f72b3b40a16029
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-06-21 10:58:40 -0500

    chris-mergeviews

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -26,18 +26,166 @@ export default function Login(props) {
 <!--        </body>-->
 <!--        </html>-->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Vetted - Login</title>
-    <!--    Font awesome-->
-    <script src="https://kit.fontawesome.com/ca8971332a.js" crossorigin="anonymous"></script>
-    <!--    Stylesheet-->
-    <link rel="stylesheet" href="assets/css/login.css">
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <title>Vetted - Login</title>-->
+<!--    &lt;!&ndash;    Font awesome&ndash;&gt;-->
+<!--    <script src="https://kit.fontawesome.com/ca8971332a.js" crossorigin="anonymous"></script>-->
+<!--    &lt;!&ndash;    Stylesheet&ndash;&gt;-->
+<!--    <link rel="stylesheet" href="assets/css/login.css">-->
 
-</head>
-<body>
+<!--</head>-->
+<!--<body>-->
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;1,100;1,300&display=swap");
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    body {
+        background: linear-gradient(90deg, #C7C5F4, #776BCC);
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    .login-screen {
+        height: 600px;
+        width: 360px;
+        position: relative;
+        background: linear-gradient(90deg, #5D54A4, #7C78B8);
+        box-shadow: 0px 0px 24px #5C5696;
+        cursor: crosshair;
+    }
+
+    .screen-content {
+        position: relative;
+        height: 100%;
+        z-index: 1;
+    }
+
+    .login-background {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 0;
+        -webkit-clip-path: inset(0 0 0 0);
+        clip-path: inset(0 0 0 0);
+    }
+
+    .screen-background-shapes {
+        position: absolute;
+        transform: rotate(45deg);
+    }
+
+    .background-shape1 {
+        height: 520px;
+        width: 520px;
+        background: #FFF;
+        top: -50px;
+        right: 120px;
+        border-radius: 0 72px 0 0;
+    }
+
+    .background-shape2 {
+        height: 220px;
+        width: 220px;
+        background: #6C63AC;
+        top: -172px;
+        right: 0;
+        border-radius: 32px;
+    }
+
+    .background-shape3 {
+        height: 540px;
+        width: 190px;
+        background: linear-gradient(270deg, #5D54A4, #6A679E);
+        top: -24px;
+        right: 0;
+        border-radius: 32px;
+    }
+
+    .background-shape4 {
+        height: 400px;
+        width: 200px;
+        background: #7E7BB9;
+        top: 420px;
+        right: 50px;
+        border-radius: 60px;
+    }
+
+    .login {
+        width: 340px;
+        padding: 30px;
+        padding-top: 156px;
+    }
+
+    .login-field {
+        position: relative;
+        padding: 20px 0px;
+    }
+
+    .login-icon {
+        position: absolute;
+        top: 30px;
+        color: #7875B5;
+    }
+
+    .login-input {
+        width: 75%;
+        border: none;
+        border-bottom: 3px solid #350259;
+        background: none;
+        padding: 10px;
+        padding-left: 24px;
+        font-weight: 700;
+        transition: .2s;
+    }
+
+    .login-input:active,
+    .login-input:focus,
+    .login-input:hover {
+        outline: none;
+        border-bottom-color: #6A679E;
+    }
+
+    .login-submit {
+        background: #fff;
+        font-size: 14px;
+        margin-top: 30px;
+        padding: 16px 20px;
+        border-radius: 26px;
+        border: 1px solid #D4D3E8;
+        text-transform: uppercase;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        color: #4C489D;
+        box-shadow: 0px 2px 2px #5C5696;
+        cursor: pointer;
+        transition: .2s;
+    }
+
+    .login-submit:active,
+    .login-submit:focus,
+    .login-submit:hover {
+        border-color: #6A679E;
+        outline: none;
+    }
+</style>
 <div class="container">
     <div class="login-screen">
         <div class="screen-content">
@@ -68,8 +216,7 @@ export default function Login(props) {
         </div>
     </div>
 </div>
-</body>
-</html>
+
     `;
 
 }
--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -29,14 +29,87 @@ export default function Register() {
 <!--        </html>-->
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Vetted - Register</title>
-    <link rel="stylesheet" href="./assets/css/register.css">
-</head>
-<body>
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <title>Vetted - Register</title>-->
+<!--    <link rel="stylesheet" href="./assets/css/register.css">-->
+<!--</head>-->
+<!--<body>-->
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;1,100;1,300&display=swap");
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+        cursor: default;
+    }
+
+    body {
+        background: linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url("../imgs/registration_background.png");
+        background-position: center;
+        background-size: cover;
+    }
+
+    .container {
+        width: 360px;
+        height: 400px;
+        margin: 8% auto;
+        background: #FFFFFF;
+        border-radius: 5px;
+        position: relative;
+    }
+
+    h3{
+        text-align: center;
+        margin-bottom: 40px;
+        color: #777;
+    }
+
+    .container form{
+        width: 280px;
+        position: absolute;
+        top: 100px;
+        left: 40px;
+        cursor: crosshair;
+    }
+
+    form input{
+        width: 100%;
+        padding: 10px 5px;
+        margin: 5px 0;
+        border: 0;
+        border-bottom: 1px solid #991;
+        outline: none;
+        background: transparent;
+    }
+
+    ::placeholder{
+        color: #777;
+    }
+
+    .btn-box{
+        width: 100%;
+        height: 30px auto;
+        text-align: center;
+    }
+
+    form button{
+        width: 110px;
+        height: 35px;
+        margin: 0 10px;
+        background: linear-gradient(to right, #C0B2FF, #350259);
+        border-radius: 30px;
+        border: 0;
+        outline: none;
+        color: #fff;
+        cursor: pointer;
+    }
+</style>
 <div class="container">
     <form action="">
         <h3>Create Account</h3>
@@ -49,8 +122,8 @@ export default function Register() {
         </div>
     </form>
 </div>
-</body>
-</html>
+<!--</body>-->
+<!--</html>-->
     `;
 }
 
