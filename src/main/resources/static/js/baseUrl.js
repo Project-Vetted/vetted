135d4e8b82bfe90d4850b6ffe488ec6696db7873
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-06-30 11:29:18 -0500

    fixed /typo in baseUrl

--- src/main/resources/static/js/baseUrl.js
+++ src/main/resources/static/js/baseUrl.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8080/api/users";
-export const BASE_URL = "https://vetted.fulgentcorp.com:8080/"
+export const BASE_URL = "https://vetted.fulgentcorp.com:8080"
